0c8983f3f40dfe5e26b694462353189cd348872d
ErgoLied <marinakisa100@rambler.ru>
2024-06-18 21:18:24 +0200

    basic structure + header skeleton

---
+++ src/components/Header/HeaderComponent.tsx
@@ -0,0 +1,19 @@
+const HeaderComponent = () => {
+    return (
+        <header>
+            [toggler]
+
+            [all movies]
+
+            [genres list]
+
+            [search movies]
+
+            [user accaunt info
+                * [favourite movies list] (ADDITIONAL)
+            ]
+        </header>
+    );
+};
+
+export default HeaderComponent;
